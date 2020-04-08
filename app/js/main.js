2304e4ca64b8b07d26fd27140887a987867f3224
Julia-Kuldorova <julia.julivne@gmail.com>
2020-04-08 21:28:16 +0300

    added slider top

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "17px",
+        readOnly: true
+    });
+
+    $(".slider__inner").slick({
+        infinite: true,
+        prevArrow:
+       '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+     nextArrow:
+       '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>'
+    });
 });
